2bf90c7fe04b2bdd20245e1d4721a2d21fc171c9
Spencer Carlson <19680614+scarlson1@users.noreply.github.com>
2023-01-20 00:52:32 -0600

    Delete functions/dist directory

--- functions/dist/Callables/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sendContactEmail = exports.getPropertyDetails = void 0;
-var getPropertyDetails_1 = require("./getPropertyDetails");
-Object.defineProperty(exports, "getPropertyDetails", { enumerable: true, get: function () { return getPropertyDetails_1.getPropertyDetails; } });
-var sendContactEmail_1 = require("./sendContactEmail");
-Object.defineProperty(exports, "sendContactEmail", { enumerable: true, get: function () { return sendContactEmail_1.sendContactEmail; } });
-//# sourceMappingURL=index.js.map
