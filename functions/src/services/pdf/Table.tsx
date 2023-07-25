0a0e8389143a8d41f2f689460e40e3afb8241583
Spencer Carlson <19680614+scarlson1@users.noreply.github.com>
2023-07-25 18:20:37 -0500

    locations pdf grid

--- functions/src/services/pdf/Table.tsx
+++ functions/src/services/pdf/Table.tsx
@@ -1,181 +1,211 @@
-import { ReactNode } from 'react';
-import ReactPDF, { Text, View, StyleSheet } from '@react-pdf/renderer';
+// @ts-nocheck
+import * as React from 'react';
+import ReactPDF, { Font, Text, View, StyleSheet, Page } from '@react-pdf/renderer';
 
-import { styles } from './createPolicy';
+// https://stackoverflow.com/a/63299486/10887890
 
-const tableStyles = StyleSheet.create({
-  table: {},
-  divider: {
+Font.register({
+  family: 'Roboto',
+  src: 'https://fonts.googleapis.com/css2?family=Roboto&display=swap',
+});
+
+const styles = StyleSheet.create({
+  page: { flexDirection: 'column', padding: 25 },
+  table: {
+    fontSize: 10,
+    // width: 550,
     width: '100%',
-    height: 1,
-    backgroundColor: '#E7EBF0',
-    margin: '24px 0 24px 0',
-  },
-  textPrimary: {
-    color: '#1A2027',
-  },
-  textSecondary: {
-    color: '#3E5060',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignContent: 'stretch',
+    flexWrap: 'nowrap',
+    alignItems: 'stretch',
   },
   row: {
     display: 'flex',
     flexDirection: 'row',
-    alignItems: 'center',
-    borderBottomColor: '#E7EBF0',
+    justifyContent: 'space-around',
+    alignContent: 'stretch',
+    flexWrap: 'nowrap',
+    alignItems: 'stretch',
+    flexGrow: 1,
+    flexShrink: 0,
+    // flexBasis: 35,
+    // borderColor: '#cc0000',
+    borderColor: '#E7EBF0',
+    borderStyle: 'solid',
     borderBottomWidth: 1,
-    paddingTop: 8,
-    paddingBottom: 8,
-    paddingLeft: 12,
-    paddingRight: 12,
-    width: 'auto',
+  },
+  cell: {
+    // borderColor: '#cc0000',
+    // borderStyle: 'solid',
+    // borderWidth: 1,
+    flexGrow: 1,
+    flexShrink: 1,
+    flexBasis: 'auto', // '25%',
+    // flex: '0 0 auto',
+    whiteSpace: 'wrap',
+    alignSelf: 'stretch',
+    // alignSelf: 'center',
+    paddingVertical: 6,
+    paddingHorizontal: 8,
+    marginHorizontal: 4,
+    display: 'flex',
+    flexDirection: 'column', // 'row',
+    justifyContent: 'flex-start',
   },
   header: {
-    // fontWeight: 400,
-    fontSize: '10px',
+    backgroundColor: '#eee',
+  },
+  headerText: {
+    fontSize: 8,
     lineHeight: 1.2,
     textTransform: 'uppercase',
-    textAlign: 'left',
-    backgroundColor: 'blue',
+    fontWeight: 500,
+    color: '#1A2027', // '#1a245c',
+    // margin: 4,
+    alignSelf: 'center',
   },
-  rowItem: {
-    // width: '60%',
-    fontSize: '10px',
-    lineHeight: 1.4,
-    textAlign: 'left',
-    // borderRightColor: borderColor,
-    // borderRightWidth: 1,
-    // paddingLeft: 8,
+  tableText: {
+    margin: 10,
+    fontSize: 10,
+    color: '#1A2027', // neutralDark,
   },
-  titleWrapper: {
-    backgroundColor: '#F3F6F9',
-    padding: '8px',
-    width: '100%',
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  title: {
-    fontSize: '12px',
-    color: '#1A2027',
-    textTransform: 'uppercase',
-    textAlign: 'center',
+  breakLongWords: {
+    whiteSpace: 'normal',
+    overflowWrap: 'break-word',
+    wordWrap: 'break-word',
+    wordBreak: 'break-all',
+    hyphens: 'auto',
   },
 });
 
-interface TableProps extends ReactPDF.ViewProps {
-  // containerProps?: Partial<ReactPDF.ViewProps>;
-  children: ReactNode;
+export interface ColumnDef {
+  field: string;
+  headerName: string;
+  flex?: number;
+  minWidth?: number;
+  alignHeader?: 'left' | 'right' | 'center';
+  alignContent?: 'left' | 'right' | 'center';
 }
 
-export const Table = ({ children, ...props }: TableProps) => {
+interface TableProps {
+  columns: ColumnDef[];
+  data: Record<string, any>[];
+}
+
+export const Table = ({ columns, data }: TableProps) => {
+  const flexBasis = `${(100 / columns.length).toFixed(2)}%`;
+  const width = `${(100 / columns.length).toFixed(2)}%`;
+
   return (
-    <View {...(props || {})} style={{ ...tableStyles.table, ...(props?.style || {}) }}>
-      {children}
+    <View style={styles.table}>
+      <View style={[styles.row, styles.header]}>
+        {columns.map((c) => {
+          const cellStyle = { width };
+
+          // if (c.flex) cellStyle['flex'] = `${c.flex} 0 ${flexBasis}`; // auto
+          if (c.flex) {
+            cellStyle['flexBasis'] = `${flexBasis}`;
+            cellStyle['flexGrow'] = c.flex;
+            cellStyle['flexShrink'] = 0;
+          }
+          if (c.minWidth) cellStyle['minWidth'] = `${c.minWidth}px`;
+          // TODO: FIX ALIGN HEADER/CONTENT
+          if (c.alignHeader) {
+            cellStyle['textAlign'] = c.alignHeader;
+          }
+
+          return (
+            <Text style={[styles.cell, styles.headerText, cellStyle]} key={c.field}>
+              {c.headerName}
+            </Text>
+          );
+        })}
+      </View>
+      {data.map((r, i) => {
+        let rowKeyVal = Object.entries(r);
+        return (
+          <View style={[styles.row]} key={`table-row-${i}`}>
+            {rowKeyVal.map(([key, val]) => {
+              let cellDef = columns.find((col) => col.field === key);
+              const cellStyle = { width }; // flexBasis
+
+              // if (cellDef.flex)
+              //   cellStyle['flex'] = `${cellDef.flex} 0 ${flexBasis}`;
+              if (cellDef.flex) {
+                cellStyle['flexBasis'] = `${flexBasis}`;
+                cellStyle['flexGrow'] = cellDef?.flex ?? 1;
+                cellStyle['flexShrink'] = 0;
+              }
+              if (cellDef.minWidth) cellStyle['minWidth'] = `${cellDef.minWidth ?? 100}px`;
+              if (cellDef.alignContent) cellStyle['textAlign'] = cellDef.alignContent;
+
+              return <Text style={[styles.cell, cellStyle, styles.breakLongWords]}>{val}</Text>;
+            })}
+          </View>
+        );
+      })}
     </View>
   );
 };
 
-interface TableHeaderProps {
-  title: string;
-  align?: 'left' | 'right' | 'center';
-  weight?: number;
+const locationColumns: ColumnDef[] = [
+  {
+    field: 'address',
+    headerName: 'Address',
+    minWidth: 140,
+  },
+  {
+    field: 'locationId',
+    headerName: 'Location ID',
+    minWidth: 120,
+  },
+  {
+    field: 'annualPremium',
+    headerName: 'Annual Premium',
+  },
+  {
+    field: 'termPremium',
+    headerName: 'Term Premium',
+  },
+  {
+    field: 'deductible',
+    headerName: 'Deductible',
+  },
+  {
+    field: 'limitA',
+    headerName: 'Building Cov.',
+  },
+  {
+    field: 'limitB',
+    headerName: 'Appt. Struct. Cov.',
+  },
+  {
+    field: 'limitC',
+    headerName: 'Contents Cov.',
+  },
+  {
+    field: 'limitD',
+    headerName: 'BI Cov.',
+    alignHeader: 'right',
+    alignContent: 'right',
+  },
+];
+
+export interface LocationsData {
+  address: string;
+  locationId: string;
+  limitA: string;
+  limitB: string;
+  limitC: string;
+  limitD: string;
+  deductible: string;
+  annualPremium: string;
+  termPremium: string;
 }
 
-export const TableHeader = ({ title, align = 'left', weight = 1 }: TableHeaderProps) => {
-  const itemStyle: ReactPDF.TextProps['style'] = { textAlign: align, flex: `${weight} 0 auto` };
-
-  return <View style={{ ...tableStyles.header, ...itemStyle }}>{title}</View>;
-};
-
-// TODO: decide whether to use widthPct or weight (--> flexGrow)
-interface TableRowProps {
-  // key: string;
-  heightPx?: number;
-  widthPct?: number;
-  children: ReactNode;
-}
-// ref: https://github.com/enescang/react-pdf-table/blob/main/src/components/TableRow/index.js
-export const TableRow = ({ children, heightPx, widthPct }: TableRowProps) => {
-  let rowStyle: ReactPDF.ViewProps['style'] = {};
-
-  if (widthPct) rowStyle.width = `${widthPct}%`;
-
-  if (heightPx) rowStyle.height = `${heightPx}px`;
-
-  return <View style={{ ...tableStyles.row, ...rowStyle }}>{children}</View>;
-};
-
-interface TableItem extends ReactPDF.TextProps {
-  text: string | number | null | undefined;
-  align?: 'left' | 'right' | 'center';
-  weight?: number;
-}
-
-export const TableItem = ({ text, style, align = 'left', weight = 1, ...props }: TableItem) => {
-  const itemStyle: ReactPDF.TextProps['style'] = { textAlign: align }; // flex: `${weight} 0 auto`
-
-  // return (
-  //   <Text
-  //     {...props}
-  //     style={{ ...tableStyles.rowItem, ...styles.textSecondary, ...(style || {}), ...itemStyle }}
-  //   >
-  //     {text || ''}
-  //   </Text>
-  // );
-  return (
-    <View style={{ flex: `${weight} 0 auto` }}>
-      <Text
-        {...props}
-        style={{ ...tableStyles.rowItem, ...styles.textSecondary, ...(style || {}), ...itemStyle }}
-      >
-        {text || ''}
-      </Text>
-    </View>
-  );
-};
-
-interface TableTitleProps {
-  title: string;
-}
-export const TableTitle = ({ title }: TableTitleProps) => {
-  return (
-    <View style={tableStyles.titleWrapper}>
-      <Text style={tableStyles.title}>{title}</Text>
-    </View>
-  );
-};
-
-interface LocationsTableProps {
-  locations: { test1: string; test2: string; test3: string }[];
-}
-
-export const LocationsTable = ({ locations }: LocationsTableProps) => {
-  return (
-    <Table>
-      <TableRow>
-        <TableHeader title='Test Title' />
-        <TableHeader title='Test Title 2' />
-        <TableHeader title='Test Title 3' align='right' />
-      </TableRow>
-      {locations.map((l) => (
-        <TableRow>
-          <TableItem text={l.test1} />
-          <TableItem text={l.test2} />
-          <TableItem text={l.test3} align='right' />
-        </TableRow>
-      ))}
-    </Table>
-  );
-};
-
-type LocationsSectionProps = LocationsTableProps;
-
-export const LocationsSection = ({ locations }: LocationsSectionProps) => {
-  return (
-    <View>
-      <TableTitle title='Locations' />
-      <LocationsTable locations={locations} />
-    </View>
-  );
+export const LocationsTable = ({ data }: { data: LocationsData[] }) => {
+  return <Table columns={locationColumns} data={data} />;
 };
