e626842fbaa57e75bef905df8d62492b91922b7c
Spencer Carlson <19680614+scarlson1@users.noreply.github.com>
2023-08-17 17:32:29 -0500

    server datagrid initial sort bug

--- client/src/hooks/useGridServerSort.ts
+++ client/src/hooks/useGridServerSort.ts
@@ -1,18 +1,20 @@
 import { GridInitialState, GridSortModel } from '@mui/x-data-grid';
 import { QueryOrderByConstraint, orderBy } from 'firebase/firestore';
-import { startTransition, useCallback, useEffect, useRef, useState } from 'react';
+import { startTransition, useCallback, useRef, useState } from 'react';
 
 import { getFirestoreSortOps } from 'modules/muiGrid';
 
 export const useGridServerSort = (initialState?: GridInitialState | undefined) => {
   const [sortModel, setSortModel] = useState<GridSortModel>(initialState?.sorting?.sortModel || []);
 
-  const sortOps = useRef<QueryOrderByConstraint[]>([]);
+  const sortOps = useRef<QueryOrderByConstraint[]>([
+    ...getFirestoreSortOps(initialState?.sorting?.sortModel),
+  ]);
 
   // initialize sortOps converting modelState to firestore sorting constraints
-  useEffect(() => {
-    sortOps.current = getFirestoreSortOps(initialState?.sorting?.sortModel);
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+  // useEffect(() => {
+  //   sortOps.current = getFirestoreSortOps(initialState?.sorting?.sortModel);
+  // }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   const handleSortModelChange = useCallback((sortModel: GridSortModel) => {
     let newOptions: QueryOrderByConstraint[] = [];
